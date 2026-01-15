000cebea886608ecb556ea94a2b57492ec5ad6e3
Youlei Yang <youlei.yang@intel.com>
2026-01-15 09:28:44 +0800

    add padding abs and ratio limits for the linear bucketing
    Signed-off-by: Youlei Yang <youlei.yang@intel.com>

--- vllm_gaudi/extension/bucketing/linear.py
+++ vllm_gaudi/extension/bucketing/linear.py
@@ -1,8 +1,5 @@
-import itertools
-import operator
 import os
 import math
-from dataclasses import dataclass, field
 from typing import List, Tuple
 
 from vllm_gaudi.extension.logger import logger as logger
@@ -13,30 +10,46 @@ class LinearBucketingStrategy:
 
     def get_prompt_cfgs(self, max_num_prefill_seqs, block_size, max_num_batched_tokens, max_model_len):
         use_merged_prefill = get_config().merged_prefill
-        prefix_caching = get_config().prefix_caching
 
-        prompt_bs_bucket_cfg = read_bucket_settings('prompt', 'bs', min=1, step=1, max=max_num_prefill_seqs)
+        prompt_bs_bucket_cfg = read_bucket_settings('prompt',
+                                                    'bs',
+                                                    min=1,
+                                                    step=2,
+                                                    max=max_num_prefill_seqs,
+                                                    pad_max=16,
+                                                    pad_percent=25)
         prompt_query_bucket_cfg = read_bucket_settings('prompt',
                                                        'query',
                                                        min=block_size,
                                                        step=block_size,
-                                                       max=max_num_batched_tokens)
+                                                       max=max_num_batched_tokens,
+                                                       pad_max=max_num_batched_tokens,
+                                                       pad_percent=25)
         max_ctx = math.ceil((max_model_len - prompt_query_bucket_cfg[0]) // block_size)
-        prompt_ctx_bucket_cfg = read_bucket_settings('prompt', 'ctx', min=0, step=1, max=max_ctx)
+        prompt_ctx_bucket_cfg = read_bucket_settings('prompt',
+                                                     'ctx',
+                                                     min=0,
+                                                     step=2,
+                                                     max=max_ctx,
+                                                     pad_max=max_num_batched_tokens // block_size,
+                                                     pad_percent=25)
 
         if use_merged_prefill:
             prev_prompt_bs_bucket_cfg = tuple(prompt_bs_bucket_cfg)
             prev_prompt_query_bucket_cfg = tuple(prompt_query_bucket_cfg)
             prev_prompt_ctx_bucket_cfg = tuple(prompt_ctx_bucket_cfg)
 
-            prompt_bs_bucket_cfg = (1, 1, 1)
-            query_min, query_step, _ = prev_prompt_query_bucket_cfg
-            prompt_query_bucket_cfg = (query_min, query_step * 4, max_num_batched_tokens)
+            prompt_bs_bucket_cfg = (1, 1, 1, prev_prompt_bs_bucket_cfg[-2], prev_prompt_bs_bucket_cfg[-1])
+            query_min, query_step, _, query_pad_max, query_pad_percent = prev_prompt_query_bucket_cfg
+            prompt_query_bucket_cfg = (query_min, query_step * 4, max_num_batched_tokens, query_pad_max,
+                                       query_pad_percent)
             prompt_ctx_bucket_cfg = read_bucket_settings('prompt',
                                                          'ctx',
                                                          min=0,
                                                          step=4,
-                                                         max=max_ctx * max_num_prefill_seqs)
+                                                         max=max_ctx * max_num_prefill_seqs,
+                                                         pad_max=max_num_batched_tokens // block_size,
+                                                         pad_percent=25)
 
             msg = ('Merged prefill is enabled!\n'
                    'Overriding prompt bucketing settings!\n'
@@ -45,7 +58,7 @@ class LinearBucketingStrategy:
                    f'prompt ctx cfg: {prev_prompt_ctx_bucket_cfg} -> {prompt_ctx_bucket_cfg}\n')
             logger().info(msg)
 
-        msg = ("Prompt bucket config (min, step, max_warmup) "
+        msg = ("Prompt bucket config (min, step, max_warmup, pad_max, pad_percent) "
                f"bs:{prompt_bs_bucket_cfg}, "
                f"query:{prompt_query_bucket_cfg}, "
                f"blocks:{prompt_ctx_bucket_cfg}")
@@ -54,15 +67,26 @@ class LinearBucketingStrategy:
         return prompt_bs_bucket_cfg, prompt_query_bucket_cfg, prompt_ctx_bucket_cfg
 
     def get_decode_cfgs(self, max_num_seqs, block_size, max_num_batched_tokens, max_model_len, max_blocks):
-        prefix_caching = get_config().prefix_caching
         contiguous_pa = get_config().use_contiguous_pa
 
-        decode_bs_bucket_cfg = read_bucket_settings('decode', 'bs', min=1, step=32, max=max_num_seqs)
-        decode_query_bucket_cfg = [1, 1, 1]
+        decode_bs_bucket_cfg = read_bucket_settings('decode',
+                                                    'bs',
+                                                    min=1,
+                                                    step=2,
+                                                    max=max_num_seqs,
+                                                    pad_max=32,
+                                                    pad_percent=25)
+        decode_query_bucket_cfg = [1, 1, 1, 1, 1]
         max_decode_blocks = max(math.ceil(max_model_len * max_num_seqs // block_size), block_size)
         if contiguous_pa:
             max_decode_blocks = max_blocks
-        decode_block_bucket_cfg = read_bucket_settings('decode', 'block', min=1, step=block_size, max=max_decode_blocks)
+        decode_block_bucket_cfg = read_bucket_settings('decode',
+                                                       'block',
+                                                       min=block_size,
+                                                       step=block_size,
+                                                       max=max_decode_blocks,
+                                                       pad_max=max_num_batched_tokens * max_num_seqs // block_size,
+                                                       pad_percent=25)
         if decode_block_bucket_cfg[2] > max_blocks:
             logger().info(
                 f'VLLM_DECODE_BLOCK_BUCKET_MAX={decode_block_bucket_cfg[2]} is higher than max_blocks={max_blocks}. Your configuration VLLM_DECODE_BLOCK_BUCKET_MAX={decode_block_bucket_cfg[2]} will be overwritten to VLLM_DECODE_BLOCK_BUCKET_MAX={max_blocks}'
@@ -75,7 +99,7 @@ class LinearBucketingStrategy:
                 )
                 decode_block_bucket_cfg[0] = decode_block_bucket_min
 
-        msg = ("Decode bucket config (min, step, max_warmup) "
+        msg = ("Decode bucket config (min, step, max_warmup, pad_max, pad_percent) "
                f"bs:{decode_bs_bucket_cfg}, "
                f"blocks:{decode_block_bucket_cfg}")
         logger().info(msg)
@@ -83,7 +107,7 @@ class LinearBucketingStrategy:
         return decode_bs_bucket_cfg, decode_query_bucket_cfg, decode_block_bucket_cfg
 
     def get_range(self, cfg):
-        range_for_cfg = warmup_range(cfg)
+        range_for_cfg = warmup_range_with_limits(cfg)
         return sorted(range_for_cfg)
 
 
@@ -95,7 +119,7 @@ def read_bucket_settings(phase: str, dim: str, **defaults):
     param is either 'min', 'step' or 'max'
     example env variable: VLLM_DECODE_BS_BUCKET_STEP=128
     """
-    params = ['min', 'step', 'max']
+    params = ['min', 'step', 'max', 'pad_max', 'pad_percent']
     env_vars = [f'VLLM_{phase}_{dim}_BUCKET_{p}'.upper() for p in params]
     default_values = [defaults[p] for p in params]
     values = []
@@ -119,35 +143,62 @@ def read_bucket_settings(phase: str, dim: str, **defaults):
     return values
 
 
-def warmup_range(config: Tuple[int, int, int]):
-    """Generate a warmup range.
+def warmup_range_with_limits(config: Tuple[int, int, int, int, int]) -> List[int]:
+    """Generate a warmup range with absolute and relative padding limits.
 
-    Start from bmin and multiply by 2 until you reach bstep.
-    Then, increase the values in the range by the value of bstep until you 
-    reach bmax.
+    1. Starts from `bucket_min` and multiply by 2 (or +1 for 0) till to `bucket_step`.
+    2. Add `bucket_step` to the values till to `bucket_max` and choose current bucket if:
+        a. the next bucket exceeds the absolute padding limit `pad_max`,
+        b. or the next bucket exceeds the padding ratio limit `pad_percent`,
+        c. or the current bucket is a multiple of `pad_max`.
+    3. Always include `bucket_max` as the last bucket.
 
     Example:
-    bmin = 2, bstep = 32, bmax = 64
-    => ramp_up = (2, 4, 8, 16)
-    => stable = (32, 64)
-    => return ramp_up + stable => (2, 4, 8, 16, 32, 64)
+    1. for config = (0, 8, 64, 64, 0)
+        ramp_up = [0, 1, 2, 4, 8]
+        stable = [16, 24, 32, 40, 48, 56, 64]
+        return [0, 1, 2, 4, 8, 16, 24, 32, 40, 48, 56, 64]
+    2. for config = (0, 8, 64, 64, 50)
+        ramp_up = [0, 1, 2, 4, 8]
+        stable = [16, 24, 32, 48, 64]  # 40 and 56 are skipped due to padding ratio limit
+        return [0, 1, 2, 4, 8, 16, 24, 32, 48, 64]
+    3. for config = (0, 8, 64, 16, 50)
+        ramp_up = [0, 1, 2, 4, 8]
+        stable = [16, 32, 48, 64]  # 24, 40, 56 are skipped due to absolute padding limit
+        return [0, 1, 2, 4, 8, 16, 32, 48, 64]
+    4. for config = (16, 16, 128, 32, 25)
+        stable = [16, 32, 48, 64, 80, 96, 112, 128]  # no ramp up phase
+        return [16, 32, 48, 64, 80, 96, 112, 128]
     """
-    bmin, bstep, bmax = config
-    add_zero_bucket = bmin == 0
-    assert bmin <= bmax, ("Min. batch size cannot be greater than max. "
-                          "batch size. If you want to skip warmup, "
-                          "set VLLM_SKIP_WARMUP=true")
-    if add_zero_bucket:
-        if bmin == 0 and bmax == 0:
-            return [0]
-        bmin = bstep
-    base = itertools.repeat(2)
-    ramp_up_acc = itertools.accumulate(base, func=operator.mul, initial=bmin)
-    ramp_up_tw = itertools.takewhile(lambda x: x < bstep and x <= bmax, \
-        ramp_up_acc)
-    stable = range(bstep, bmax + 1, bstep)
-    buckets = list(ramp_up_tw) + list(stable)
-    buckets = [b for b in buckets if b >= bmin]
-    if add_zero_bucket:
-        buckets.append(0)
-    return list(buckets)
+    bucket_min, bucket_step, bucket_max, pad_max, pad_percent = config
+    assert bucket_min <= bucket_max, ("bucket_min cannot be greater than bucket_max. "
+                                      "If you want to skip warmup, set VLLM_SKIP_WARMUP=true")
+    assert bucket_step > 0, f"bucket_step must be positive, got: ({bucket_step})"
+    assert 0 <= pad_percent <= 50, f"pad_percent must be between 0 and 50 percentage points, got: ({pad_percent})"
+
+    buckets = [bucket_min]
+    current_bucket = bucket_min
+    while current_bucket <= bucket_max:
+        last_bucket = buckets[-1]
+        if current_bucket <= bucket_step:
+            next_bucket = last_bucket * 2
+            if next_bucket == 0:
+                next_bucket += 1
+            if next_bucket <= bucket_max:
+                buckets.append(next_bucket)
+        else:
+            next_bucket = current_bucket + bucket_step
+            max_padding = next_bucket - last_bucket - 1
+            max_padding_ratio = max_padding / next_bucket
+            keep_bucket = (
+                max_padding_ratio > pad_percent / 100.0  # next bucket exceeds padding ratio limit
+                or max_padding > pad_max  # next bucket exceeds absolute padding limit
+                or current_bucket % pad_max == 0  # current bucket is a multiple of pad_max
+            )
+            if keep_bucket and current_bucket != last_bucket:
+                buckets.append(current_bucket)
+        current_bucket = next_bucket
+    if buckets[-1] != bucket_max:
+        buckets.append(bucket_max)
+
+    return buckets
